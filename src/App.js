bca75a3a7774e3bf1a09c58c93812d550af79f42
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-07-07 15:33:05 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
-
 import Choose from './pages/Choose';
+import { useState } from 'react';
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState()
   return (
    <BrowserRouter>
    <Routes>
-    <Route path = "/" element= {< Choose/>}/>
+    <Route path = "/" element= {< Choose setSelectedItem={setSelectedItem} selectedItem={selectedItem}/>}/>
    </Routes>
    
    
--- src/pages/Choose.jsx
+++ src/pages/Choose.jsx
@@ -1,9 +1,17 @@
 import AppLayout from "./layout/AppLayout"
 
-export default function Choose(){
+export default function Choose({setSelectedItem, selectedItem}){
+
+    
     return(
         <AppLayout>
             <h1>Player VS Computer</h1>
+            <div>
+             <img src="/images/paper.png" onClick={() => setSelectedItem(0)} />
+             <img src= "/images/leftScissor.png" onClick={() =>setSelectedItem(1)}/>
+             <img src="/images/rockRight.png"  onClick={() =>setSelectedItem(2)}/>
+            </div>
+            <button>Pick one!</button>
         </AppLayout>
     )
 
